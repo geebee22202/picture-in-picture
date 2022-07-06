d8567f363871933fa00cab611e65c39726512d5c
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-07-06 14:57:50 -0400

    added functionality to display screen recorded videos

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
 
+//Promtp user to select media stream, pass to video element, then play
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {}
+}
+
+button.addEventListener("click", async () => {
+  // Disable button
+  button.disabled = true;
+  // Start picture in picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
+
+// On load
+selectMediaStream();
